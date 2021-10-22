5596543ab0f902dc3302289488c5d9efd928b257
Reena Mendonsa <reenamendonsa@gmail.com>
2021-10-22 17:15:22 +0530

    Quiz app

---
+++ block-BJaaim/data.js
@@ -0,0 +1,105 @@
+const questionnire = [
+    {
+        title: "Which is the capital of India ?",
+        options: [
+            "Delhi",
+            "Mumbai",
+            "Kolkata",
+            "Uttar Pradesh"
+        ],
+        answerIndex: 0
+    },
+
+    {
+        title: "Who is the president of India ?",
+        options: [
+            "Pranab Mukharjee",
+            "Gulab Nabi Azad",
+            "Mamta Benarjee",
+            "Ram Nath Govind"
+        ],
+        answerIndex: 3
+    },
+
+    {
+        title: "Who is the prime minister of India?",
+        options: [
+            "Nitin Gadkari",
+            "Amit Shah",
+            "Narendra Modi",
+            "Yogi Aditya Nath"
+        ],
+        answerIndex: 2
+    },
+    {
+        title: "National Animal of India",
+        options: [
+            "tiger",
+            "lion",
+            "zebra",
+            "leopard"
+        ],
+        answerIndex: 0
+    },
+    {
+        title: "National bird of India",
+        options: [
+            "cuckoo",
+            "maina",
+            "peacock",
+            "parrot "
+        ],
+        answerIndex: 2
+    },
+    {
+        title: "National flower of India",
+        options: [
+            "Rose",
+            "Jasmine",
+            "Marigold",
+            "Lotus"
+        ],
+        answerIndex: 3
+    },
+    {
+        title: "Which state is famous for its beaches",
+        options: [
+            "West Bengal",
+            "Odisa",
+            "goa",
+            "karnataka"
+        ],
+        answerIndex: 2
+    },
+    {
+        title: "Pearl city is ?",
+        options: [
+            "Hyderabad",
+            "Aurangabad",
+            "Chennai",
+            "Mumbai"
+        ],
+        answerIndex: 0
+    },
+    {
+        title: "Capital of karnatak is?",
+        options: [
+            "Bhopal",
+            "Bengaluru",
+            "Patna",
+            "Chennai"
+        ],
+        answerIndex: 1
+    },
+    {
+        title: "Nathula pass is in?",
+        options: [
+            "Mizoram",
+            "Kanpur",
+            "Sikkim",
+            "Assam"
+        ],
+        answerIndex: 2
+    },
+
+]
